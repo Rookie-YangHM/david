71cf73b5c9d5dbaab58a7a07b708141f25651198
Rookie-YangHM <yangheemin+codyssey@gmail.com>
2025-09-26 17:44:47 +0900

    Update app.py @githubWeb

--- mission/01.devops_2_1_/app.py
+++ mission/01.devops_2_1_/app.py
@@ -11,10 +11,16 @@ def home():
         hostname = ' '
     return render_template('index.html', computername=hostname)
 
+"""
 # 새로운 메뉴 라우트 추가
 @app.route('/menu')
 def menu():
     return render_template('menu.html')
+"""
+
+@app.route("/test2")
+def test2():
+    return render_template('test2.html')
 
 if __name__ == "__main__":
 	app.run('0.0.0.0',port=5000,debug=True)
