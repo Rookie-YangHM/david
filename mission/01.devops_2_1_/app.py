cf604bf17f53629ed4e28087cd86f9620f1cd31c
H.M. YANG @ codyssey : vscode : ubuntu <yangheemin+codyssey@gmail.com>
2025-09-26 17:14:54 +0900

    work for 2_1

---
+++ mission/01.devops_2_1_/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+import socket
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    if app.debug:
+        hostname = '컴퓨터(인스턴스) : ' + socket.gethostname()
+    else:
+        hostname = ' '
+    return render_template('index.html', computername=hostname)
+
+# 새로운 메뉴 라우트 추가
+@app.route('/menu')
+def menu():
+    return render_template('menu.html')
+
+if __name__ == "__main__":
+	app.run('0.0.0.0',port=5000,debug=True)
