1c8298f804960ecf516bdb317f296ade811453de
H.M. YANG @ codyssey : vscode : ubuntu <yangheemin+codyssey@gmail.com>
2025-09-26 17:43:54 +0900

    change add.py

--- mission/01.devops_2_1_/app.py
+++ mission/01.devops_2_1_/app.py
@@ -11,10 +11,16 @@ def home():
         hostname = ' '
     return render_template('index.html', computername=hostname)
 
+"""
 # 새로운 메뉴 라우트 추가
 @app.route('/menu')
 def menu():
     return render_template('menu.html')
+"""
+
+@app.route("/test1")
+def test1():
+    return render_template('test1.html')
 
 if __name__ == "__main__":
 	app.run('0.0.0.0',port=5000,debug=True)
